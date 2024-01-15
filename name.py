9b98a94aa6ce5273d3465d72c91594b29cfe2ddd
sarathchandrasimma <sarathchandrarocking@gmail.com>
2024-01-15 23:01:30 +0530

    new feature

--- name.py
+++ name.py
@@ -5,6 +5,11 @@ a = "sarathchandra"
 #         if(i<=j):
 #             break
     
-for i in range(0,10):
-    print(f"{i+1}.my name is {a}")
-print ("thanks for visiting my github")
+# for i in range(0,10):
+#     print(f"{i+1}.my name is {a}")
+# print ("thanks for visiting my github")
+i=1
+while(i<11)
+    print(f"{i}.my name is sarathchandra")
+    i++
+print("i'm out of while loop")
