9850be30622a26661bc686a9f3000b861f3362b3
sarathchandrasimma <sarathchandrarocking@gmail.com>
2024-01-15 22:56:11 +0530

    name file in py code

---
+++ name.py
@@ -0,0 +1,10 @@
+a = "sarathchandra"
+# for j in range(0,10):
+#     for i in range(0,10):
+#         print(f"{j}my name is {a}")
+#         if(i<=j):
+#             break
+    
+for i in range(0,10):
+    print(f"{i+1}.my name is {a}")
+print ("thanks for visiting my github")
